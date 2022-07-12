c26dc2b9acd5bf7d7c3478b315f6bd1beb8b6fc1
Zhaohu Xing <x.zhaohu@gmail.com>
2022-07-12 19:08:43 +0800

    test: modify watermark case

--- tests/watermark.rs
+++ tests/watermark.rs
@@ -16,13 +16,18 @@ fn test_watermark() {
         let result = watermark_text(&buffer, "文字水印", 300, 0.32, "#FF1493");
         file.write(&result);
     }
+}
 
-    let i = "test.jpeg";
-    let mut f = File::open(format!("images/{}", i)).unwrap();
-    let mut buffer: Vec<u8> = Vec::new();
-    f.read_to_end(&mut buffer);
+#[test]
+fn test_watermark_image() {
+    let images = vec!["test.jpeg", "test.webp", "test.tiff"];
+    for i in images.iter() {
+        let mut f = File::open(format!("images/{}", i)).unwrap();
+        let mut buffer: Vec<u8> = Vec::new();
+        f.read_to_end(&mut buffer);
 
-    let mut file = File::create(format!("images/watermark_image_{}", i)).unwrap();
-    let result = watermark_image(&buffer, &buffer, 0, 0, 0);
-    file.write(&result);
+        let mut file = File::create(format!("images/watermark_image_{}", i)).unwrap();
+        let result = watermark_image(&buffer, &buffer, 0, 0, 0);
+        file.write(&result);
+    }
 }
